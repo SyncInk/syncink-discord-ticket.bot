5218e71a8ed4be606ecf97dc3b0c16d5a8109696
SyncInk <briturag@gmail.com>
2026-06-07 15:27:42 +0530

    fix: remove deprecated mongoose options

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,10 +7,7 @@ async function initDatabase() {
         console.error('[DB] MONGO_URI is missing in .env file!');
         process.exit(1);
     }
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('[DB] Connected to MongoDB');
 }
 
