2a4412262df4d6ed677dbaa29b6e35cb8e3645ce
SyncInk <briturag@gmail.com>
2026-06-08 06:46:53 +0530

    feat: add support for claiming on behalf of mentioned staff members

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -14,9 +14,20 @@ module.exports = {
 
         const guildConfig = await db.getGuildConfig(message.guild.id);
 
-        const isStaff = [...guildConfig.staffRoleIds, ...guildConfig.adminRoleIds, ...guildConfig.ownerRoleIds, ...guildConfig.developerRoleIds].some(roleId => message.member.roles.cache.has(roleId)) || message.member.permissions.has(PermissionFlagsBits.Administrator);
+        const staffRoles = [...guildConfig.staffRoleIds, ...guildConfig.adminRoleIds, ...guildConfig.ownerRoleIds, ...guildConfig.developerRoleIds];
+        const isStaff = staffRoles.some(roleId => message.member.roles.cache.has(roleId)) || message.member.permissions.has(PermissionFlagsBits.Administrator);
 
         if (isStaff) {
+            const newClaimers = [message.author.id];
+            
+            message.mentions.members.forEach(member => {
+                if (member.user.bot) return;
+                const isMentionedStaff = staffRoles.some(roleId => member.roles.cache.has(roleId)) || member.permissions.has(PermissionFlagsBits.Administrator);
+                if (isMentionedStaff && !newClaimers.includes(member.id)) {
+                    newClaimers.push(member.id);
+                }
+            });
+
             try {
                 const messages = await message.channel.messages.fetch({ limit: 10, after: '0' });
                 const welcomeMsg = messages.find(m => m.author.id === client.user.id && m.embeds.length > 0 && m.embeds[0].title === 'Claimers');
@@ -24,18 +35,24 @@ module.exports = {
                 if (welcomeMsg) {
                     const claimersEmbed = EmbedBuilder.from(welcomeMsg.embeds[0]);
                     let desc = claimersEmbed.data.description || '';
-                    
-                    if (desc.includes('No one has claimed')) {
-                        desc = `• <:claimers:1513345698689581087> <@${message.author.id}>`;
+                    let updated = false;
+
+                    for (const claimerId of newClaimers) {
+                        if (desc.includes('No one has claimed')) {
+                            desc = `• <:claimers:1513345698689581087> <@${claimerId}>`;
+                            await message.channel.send(`<:claimers:1513345698689581087> <@${claimerId}> is a claimer now!`);
+                            await db.updateTicket(message.channel.id, { claimerId: claimerId });
+                            updated = true;
+                        } else if (!desc.includes(claimerId)) {
+                            desc += `\n• <:claimers:1513345698689581087> <@${claimerId}>`;
+                            await message.channel.send(`<:claimers:1513345698689581087> <@${claimerId}> is also a claimer now!`);
+                            updated = true;
+                        }
+                    }
+
+                    if (updated) {
                         claimersEmbed.setDescription(desc);
                         await welcomeMsg.edit({ embeds: [claimersEmbed, welcomeMsg.embeds[1]] });
-                        await message.channel.send(`<:claimers:1513345698689581087> <@${message.author.id}> is a claimer now!`);
-                        await db.updateTicket(message.channel.id, { claimerId: message.author.id });
-                    } else if (!desc.includes(message.author.id)) {
-                        desc += `\n• <:claimers:1513345698689581087> <@${message.author.id}>`;
-                        claimersEmbed.setDescription(desc);
-                        await welcomeMsg.edit({ embeds: [claimersEmbed, welcomeMsg.embeds[1]] });
-                        await message.channel.send(`<:claimers:1513345698689581087> <@${message.author.id}> is also a claimer now!`);
                     }
                 }
             } catch (err) {
